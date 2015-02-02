5a8a599fbb286797ddea4d9b2734aa983ddc4d13
App Academy Student <student@appacademy.io>
2015-02-02 11:13:16 -0800

    item/card models/collections

--- app/assets/javascripts/models/card.js
+++ app/assets/javascripts/models/card.js
@@ -1,4 +1,17 @@
 TrelloClone.Models.Card = Backbone.Model.extend({
   urlRoot: 'api/cards',
 
+  cards: function() {
+    this._cards = this._cards || new TrelloClone.Collections.Cards([], { list: this});
+    return this._lists;
+  },
+
+  parse: function (payload) {
+    if (payload.cards) {
+      this.lists().set(payload.cards, { parse: true });
+      delete payload.cards;
+    }
+    return payload;
+  }
+
 })
--- app/views/api/boards/_lists.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.(list, :id, :title, :ord)
